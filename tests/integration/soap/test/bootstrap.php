ebb5d8e2d9eb4e0e0816defed127c7fe58a4206f
Andrius Petrauskis <andrius@petrauskis.com>
2020-01-27 23:54:43 +0200

    Exclude phpunit filesystem from replacing

--- tests/integration/soap/test/bootstrap.php
+++ tests/integration/soap/test/bootstrap.php
@@ -2,5 +2,8 @@
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 $loader = require_once 'vendor/autoload.php';
 
+\VCR\VCR::configure()->setBlackList([
+    'phpunit/phpunit/Util/Filesystem.php',
+]);
 \VCR\VCR::turnOn();
 \VCR\VCR::turnOff();
